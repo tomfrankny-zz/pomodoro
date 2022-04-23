a4ae7472b53b3ded79e0665f2df3343afc4736ee
“tomfrankny” <tomcichra@gmail.com>
2022-04-23 03:49:19 -0300

    Updates

---
+++ precache-manifest.6c3993432cafd7d71d38d24f4ea90ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8354acb18643497bbf53",
+    "url": "/pomodoro/static/css/main.2e339c86.chunk.css"
+  },
+  {
+    "revision": "8354acb18643497bbf53",
+    "url": "/pomodoro/static/js/main.8354acb1.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/pomodoro/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "5ea65d3e8baaa64cb7ac",
+    "url": "/pomodoro/static/js/2.5ea65d3e.chunk.js"
+  },
+  {
+    "revision": "2440b05751a69e542307",
+    "url": "/pomodoro/static/js/runtime~main.2440b057.js"
+  },
+  {
+    "revision": "232cace3abe2bd77b0358a0457b351f3",
+    "url": "/pomodoro/index.html"
+  }
+];
